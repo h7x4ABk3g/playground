513d4e06d5da9b4dccc809d87c1cd0bed1636d68
h7x4 <h7x4abk3g@protonmail.com>
2021-02-11 17:19:41 +0100

    Part 2 of the moving

--- python/ctypes/square_c/c.py
+++
@@ -1,9 +0,0 @@
-from ctypes import CDLL
-from os import system
-
-def compile(filename):
-  system(f'cc -fPIC -shared -o {filename}.so {filename}.c')
-
-compile('square')
-c_file = CDLL('./square.so')
-print(c_file.square_c(10))
